3e02a8ceaf07bdc0d5d22143bc93542ee6a46afc
ddochi9 <rangskim@naver.com>
2020-08-14 20:22:20 +0900

    initial commit

---
+++ app/src/main/java/com/ktigers20/mez/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ktigers20.mez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
