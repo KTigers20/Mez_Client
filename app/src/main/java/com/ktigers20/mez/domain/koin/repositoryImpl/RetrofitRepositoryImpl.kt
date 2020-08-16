cd93cc2c04f55fe25a6b4941608729c397341aef
ddochi9 <rangskim@naver.com>
2020-08-17 00:06:36 +0900

    Glide 모듈 추가

--- app/src/main/java/com/ktigers20/mez/domain/koin/repositoryImpl/RetrofitRepositoryImpl.kt
+++ app/src/main/java/com/ktigers20/mez/domain/koin/repositoryImpl/RetrofitRepositoryImpl.kt
@@ -4,16 +4,19 @@ import com.ktigers20.mez.BuildConfig
 import com.ktigers20.mez.domain.globalconst.Consts
 import com.ktigers20.mez.domain.koin.repository.HttpClientRepository
 import com.ktigers20.mez.domain.koin.repository.RetrofitRepository
+import com.ktigers20.mez.domain.utils.add
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitRepositoryImpl(private val okHttpRepo: HttpClientRepository): RetrofitRepository {
+    private val versionInfo = "v1/"
+
     override fun getRefreshRetrofit(): Retrofit {
         val client = okHttpRepo.getRefreshOkHttp()
         val baseUrl: String = Consts.BASE_URL
         return Retrofit.Builder()
-            .baseUrl(baseUrl)
+            .baseUrl(baseUrl add versionInfo)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(client)
@@ -24,7 +27,7 @@ class RetrofitRepositoryImpl(private val okHttpRepo: HttpClientRepository): Retr
         val client = okHttpRepo.getAccessOkHttp()
         val baseUrl: String = Consts.BASE_URL
         return Retrofit.Builder()
-            .baseUrl(baseUrl)
+            .baseUrl(baseUrl add versionInfo)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(client)
