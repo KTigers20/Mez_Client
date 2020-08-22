a6ab50ce0b51ae1ce88cb3ca44691fa6976527aa
ddochi9 <rangskim@naver.com>
2020-08-22 20:53:54 +0900

    비상연락망 화면 구현 완료 & sharedPref module rx버전 변경 후 원상복귀

--- app/src/main/java/com/ktigers20/mez/feature/phonebook/PhonebookFragment.kt
+++ app/src/main/java/com/ktigers20/mez/feature/phonebook/PhonebookFragment.kt
@@ -6,13 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jakewharton.rxbinding2.widget.RxTextView
 import com.ktigers20.mez.R
+import com.ktigers20.mez.data.entity.PhoneBookInfo
 import com.ktigers20.mez.databinding.FragmentMainBinding
 import com.ktigers20.mez.databinding.FragmentPhonebookBinding
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import java.util.concurrent.TimeUnit
 
 class PhonebookFragment : Fragment(), PhonebookContract.View {
     private lateinit var phonebookBinding: FragmentPhonebookBinding
     private lateinit var presenter: PhonebookPresenter
+    private lateinit var mPhoneBookInfoList: ArrayList<PhoneBookInfo>
+
+    private lateinit var mPhoneBookAdapter: PhonebookAdapter
+    private val compositeDisposable = CompositeDisposable()
 
     companion object {
         @JvmStatic
@@ -39,6 +49,26 @@ class PhonebookFragment : Fragment(), PhonebookContract.View {
     }
 
     private fun initView() {
+        //1초 미만 입력시 서버 통신
+        RxTextView.textChanges(phonebookBinding.searchEditText)
+            .debounce(1000, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
 
+            }.addTo(compositeDisposable)
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()
+        super.onDestroy()
+    }
+
+
+    override fun setPhoneBookList(phoneBookList: ArrayList<PhoneBookInfo>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setPhoneBookPageIsEnd(isEnd: Boolean) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/ktigers20/mez/feature/phonebook/PhonebookPresenter.kt
+++ app/src/main/java/com/ktigers20/mez/feature/phonebook/PhonebookPresenter.kt
@@ -1,5 +1,7 @@
 package com.ktigers20.mez.feature.phonebook
 
 class PhonebookPresenter(override val view: PhonebookContract.View): PhonebookContract.Presenter {
-
+    override fun getPhoneBookList() {
+        TODO("Not yet implemented")
+    }
 }
