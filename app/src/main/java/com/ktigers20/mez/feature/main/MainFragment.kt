8c4e8dbfb2ce79b90626ea3084f8c315ff27a04c
ddochi9 <rangskim@naver.com>
2020-08-27 23:18:03 +0900

    chart 함수 추가

--- app/src/main/java/com/ktigers20/mez/feature/main/MainFragment.kt
+++ app/src/main/java/com/ktigers20/mez/feature/main/MainFragment.kt
@@ -1,13 +1,20 @@
 package com.ktigers20.mez.feature.main
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.charts.HorizontalBarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.ktigers20.mez.R
 import com.ktigers20.mez.databinding.FragmentMainBinding
+import kotlinx.android.synthetic.main.activity_search_detail.view.*
 
 
 class MainFragment : Fragment(), MainContract.View {
@@ -39,6 +46,52 @@ class MainFragment : Fragment(), MainContract.View {
     }
 
     private fun initView() {
+        setUpChart()
 
+
+    }
+
+    private fun setUpChart() {
+        val horizontalBarChart = mainBinding.horizontalBarChart
+        val noOfBatch = ArrayList<BarEntry>()
+        noOfBatch.add(BarEntry(348f, 0))
+        noOfBatch.add(BarEntry(50f, 1))
+        noOfBatch.add(BarEntry(10f, 2))
+
+        noOfBatch.add(BarEntry(10f, 3))
+        noOfBatch.add(BarEntry(10f, 4))
+        noOfBatch.add(BarEntry(10f, 5))
+
+        noOfBatch.add(BarEntry(10f, 6))
+        noOfBatch.add(BarEntry(10f, 7))
+        noOfBatch.add(BarEntry(10f, 8))
+
+
+        val category = ArrayList<String>()
+        category.add("Waiting")
+        category.add("Fail")
+        category.add("Success")
+
+        category.add("Waiting")
+        category.add("Fail")
+        category.add("Success")
+
+        category.add("Waiting")
+        category.add("Fail")
+        category.add("Success")
+
+
+        var barDataSet = BarDataSet(noOfBatch, "batch")
+        horizontalBarChart.animateY(1000)
+        var data = BarData(category, barDataSet)
+        barDataSet.colors =
+            mutableListOf(
+                Color.parseColor("#C2C7CF"),
+                Color.parseColor("#FF8181"),
+                Color.parseColor("#6FCF97")
+            )
+
+        horizontalBarChart.data = data
+        horizontalBarChart.setDescription("")
     }
 }
