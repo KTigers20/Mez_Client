6fcb742c3cb4aa9d6a719d8ecfce95e69bcbc6cf
ddochi9 <rangskim@naver.com>
2020-08-30 10:47:34 +0900

    로그인 실패처리

--- app/src/main/java/com/ktigers20/mez/feature/login/LoginPresenter.kt
+++ app/src/main/java/com/ktigers20/mez/feature/login/LoginPresenter.kt
@@ -1,12 +1,16 @@
 package com.ktigers20.mez.feature.login
 
 import android.annotation.SuppressLint
+import android.util.Log
 import com.ktigers20.mez.data.request.LoginRequest
 import com.ktigers20.mez.domain.api.usecase.GetAccessTokenUseCase
 import com.ktigers20.mez.domain.globalconst.Consts
 import com.ktigers20.mez.domain.koin.repository.SharedPrefRepository
+import com.ktigers20.mez.domain.utils.ConverterUtil
 import com.ktigers20.mez.domain.utils.addTo
 import io.reactivex.disposables.CompositeDisposable
+import org.json.JSONException
+import org.json.JSONObject
 
 class LoginPresenter(
     override val view: LoginContract.View
@@ -18,10 +22,22 @@ class LoginPresenter(
     @SuppressLint("CheckResult")
     override fun getAccessToken(id: String, password: String) {
         getAccessTokenUseCase.getAccessToken(LoginRequest(id, password)).subscribe({
-            if (it.code() == 200) {
+            val response = JSONObject(it.toString())
+            var accessToken = ""
+            try {
+                accessToken = response.getString("token")
+            } catch ( e : JSONException) {
+                view.toastError()
+                e.printStackTrace()
+            }
+            if (accessToken.isNotEmpty()) {
                 sharedPrefRepository.writePrefs(Consts.AUTO_LOGIN, true)
                 sharedPrefRepository.writePrefs(Consts.USER_ID, id)
-                view.navigateToMainPage(it.body()!!.token)
+                sharedPrefRepository.writePrefs(
+                    Consts.ACCESS_TOKEN,
+                    ConverterUtil._Encode(accessToken)
+                )
+                view.navigateToMainPage()
             } else {
                 view.toastError()
             }
